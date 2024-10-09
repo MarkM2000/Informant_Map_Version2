bd7002130788e2ac0c90c5963e99c844b17b282d
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-10-09 14:27:40 -0400

    Create unknown.js
    Create js for unknown genders

---
+++ assets/gender/unknown.js
@@ -0,0 +1,47 @@
+var unknown = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MA123",
+          "Sex": "Male/Female"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -72.068,
+            42.5555
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "ME86",
+          "Sex": "Unknown"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -70.267,
+            43.667
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA158",
+          "Sex": "Unknown"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.5946,
+            32.1785
+          ]
+        }
+      }
+    ]
+  }
