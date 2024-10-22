1e0c4c588be395850c43fa8def6fbb15134ee678
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-10-22 15:15:10 -0400

    Create fisher.js

---
+++ assets/occupation/fisher.js
@@ -0,0 +1,355 @@
+var fisher = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NJ10A",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -74.26583,
+            40.50667
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MD3A",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -76.28139,
+            38.94695
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MD20A",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -76.5125,
+            38.84167
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "VA2B",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.04723,
+            38.80472
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "VA19A",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.17889,
+            37.85472
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "VA25",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -76.46889,
+            37.16528
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NC10A",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -76.2525,
+            35.9175
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NC20A",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.59111,
+            35.06667
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NC20B",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.35306,
+            35.06694
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC4A",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -79.94417,
+            34.04028
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA4B",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -81.36972,
+            31.50917
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA7",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -81.69,
+            30.79972
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "RI23",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -71.58,
+            41.1697
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "ME302",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -67.4713,
+            44.6233
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "ME304",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -67.0758,
+            44.975
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "FL251",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -83.0353,
+            29.1417
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "AL388",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -86.5836,
+            33.1145
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "FL250",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -83.3731,
+            29.664
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "FL252",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -83.0355,
+            29.1411
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "FL452",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -84.987,
+            29.7255
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "FL454",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -85.3009,
+            29.8125
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "LA630A",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -89.7828,
+            30.2776
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MS619",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -88.9728,
+            30.3951
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "CT205",
+          "Occupation": "Fisher "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.5647,
+            41.0228
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "CT283",
+          "Occupation": "Fisher "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.0597,
+            41.2242
+          ]
+        }
+      }
+    ]
+  }
