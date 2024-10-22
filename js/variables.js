0afb6a110f3f19943a327a28c652f838f8c29e57
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-10-22 16:09:20 -0400

    Adding seeking_work, service_worker variables

--- js/variables.js
+++ js/variables.js
@@ -837,3 +837,73 @@ var public_servant = L.geoJson(public_servant, {
         });
     }
 })
+
+var seeking_work = L.geoJson(seeking_work, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#1f78b4',
+            weight: 1,
+            fillColor: '#FA8072',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+					<b>${props.Informant}</b>
+					<br>Occupation: ${props.Occupation}<br>
+				`
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#FA8072'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#FA8072'
+            });
+        });
+    }
+})
+
+var service_worker = L.geoJson(service_worker, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#1f78b4',
+            weight: 1,
+            fillColor: '#FA8072',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+					<b>${props.Informant}</b>
+					<br>Occupation: ${props.Occupation}<br>
+				`
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#FA8072'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#FA8072'
+            });
+        });
+    }
+})
